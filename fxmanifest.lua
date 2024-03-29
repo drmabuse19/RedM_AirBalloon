c602a097cbfd78f2f2a2fcb208c89f61f40f63c6
TheMabuse <40247440+drmabuse19@users.noreply.github.com>
2024-03-29 21:18:59 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,5 @@
 fx_version 'adamant'
 games { 'rdr3' }
-author 'Neo Green'
+author '6c6'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-client_script 'baloon_c.lua'
+client_script 'client.lua'
